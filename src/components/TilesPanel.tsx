cd132370aeb1517dc33b365d13b7b773f0c96480
all-you-need-is-laugh <smerost2@gmail.com>
2025-02-02 22:56:35 +0100

    Add TilesDebug component to display tile neighbors in TilesPanel

--- src/components/TilesPanel.tsx
+++ src/components/TilesPanel.tsx
@@ -7,15 +7,39 @@ const TilesPanelStyled = styled(Panel)`
   background-color: lightyellow;
 `;
 
+const StyledLabel = styled.label`
+  font-weight: bold;
+  width: 80px;
+  display: inline-block;
+`;
+
 interface TilesPanelProps {
   tiles: Tile[];
 }
+
+const TilesDebug = ({ tiles }: TilesPanelProps) => (
+  <>
+    {
+      tiles.map((tile, index) => (
+        <div key={index}>
+          <div>Tile {index}</div>
+          <StyledLabel>Top:</StyledLabel><TileSet tiles={[tile, ...tile.topNeighbors]} /><br />
+          <StyledLabel>Right:</StyledLabel><TileSet tiles={[tile, ...tile.rightNeighbors]} /><br />
+          <StyledLabel>Bottom:</StyledLabel><TileSet tiles={[tile, ...tile.bottomNeighbors]} /><br />
+          <StyledLabel>Left:</StyledLabel><TileSet tiles={[tile, ...tile.leftNeighbors]} />
+        </div>
+      ))
+    }
+  </>
+);
 
 export function TilesPanel({ tiles }: TilesPanelProps) {
   return (
     <TilesPanelStyled>
       <h2>Tiles</h2>
       <TileSet tiles={tiles} />
+      <br />
+      {/* <TilesDebug tiles={tiles} /> */}
     </TilesPanelStyled>
   )
 }
