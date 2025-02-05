1e4ea38860c257034ef57b97a813652fc73baf28
all-you-need-is-laugh <smerost2@gmail.com>
2025-02-05 22:52:57 +0100

    Add image selection dropdown to InputPanel for dynamic tile extraction

--- src/components/InputPanel.tsx
+++ src/components/InputPanel.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import Random from 'prando';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Tile } from '../entities/Tile';
 import { extractTiles } from '../utils/extractTiles';
 import { readImageData } from '../utils/readImageData';
 import Panel from './Panel';
+
+const random = new Random();
 
 const InputPanelStyled = styled(Panel)`
   background-color: lightblue;
@@ -17,26 +20,109 @@ const StyledImage = styled.img`
   image-rendering: pixelated;
 `;
 
+const StyledSelect = styled.select`
+  padding: 10px;
+  margin: 10px 0;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  font-size: 16px;
+  color: #333;
+`;
+
 interface InputPanelProps {
   onTilesExtracted: (tiles: Tile[]) => void;
 }
 
+const inputOptiopns = [
+  '3Bricks',
+  'Angular',
+  'BrownFox',
+  'Cat',
+  'Cats',
+  'Cave',
+  'Chess',
+  'Circle',
+  'City',
+  'ColoredCity',
+  'Disk',
+  'Dungeon',
+  'Fabric',
+  'Flowers',
+  'Font',
+  'Forest',
+  'Hogs',
+  'Knot',
+  'Lake',
+  'LessRooms',
+  'Lines',
+  'Link',
+  'Link2',
+  'MagicOffice',
+  'Maze',
+  'Mazelike',
+  'MoreFlowers',
+  'Mountains',
+  'Nested',
+  'NotKnot',
+  'Office',
+  'Office2',
+  'Paths',
+  'Platformer',
+  'Qud',
+  'RedDot',
+  'RedMaze',
+  'Rooms',
+  'Rule126',
+  'Sand',
+  'ScaledMaze',
+  'Sewers',
+  'SimpleKnot',
+  'SimpleMaze',
+  'SimpleWall',
+  'Skew1',
+  'Skew2',
+  'Skyline',
+  'Skyline2',
+  'SmileCity',
+  'Spirals',
+  'Town',
+  'TrickKnot',
+  'Village',
+  'Wall',
+  'WalledDot',
+  'Water',
+  'Wrinkles',
+];
+
+function imageFullPath(selectedImage: string): string {
+  return `/samples/${selectedImage}.png`;
+}
+
 export function InputPanel({ onTilesExtracted = () => { } }: InputPanelProps) {
-  const imageSrc = 'samples/City.png';
+  const [selectedImage, setSelectedImage] = useState(random.nextArrayItem(inputOptiopns));
 
   useEffect(() => {
     (async () => {
-      const imageData = await readImageData(imageSrc);
+      const imageData = await readImageData(imageFullPath(selectedImage));
       const tiles = extractTiles(imageData, 3);
 
       onTilesExtracted(tiles);
     })();
-  }, [imageSrc, onTilesExtracted]);
+  }, [selectedImage, onTilesExtracted]);
 
   return (
     <InputPanelStyled>
       <h2>Input</h2>
-      <StyledImage src={imageSrc} />
+      <StyledSelect onChange={(e) => setSelectedImage(e.target.value)} value={selectedImage}>
+        {inputOptiopns.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </StyledSelect>
+      <StyledImage src={imageFullPath(selectedImage)} />
     </InputPanelStyled>
   )
 }
+
