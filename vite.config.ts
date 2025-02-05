58d4b50706c1a64b2485d154a1cad69173cf397c
all-you-need-is-laugh <smerost2@gmail.com>
2025-02-06 00:09:58 +0100

    Update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/[REPO_NAME]/",
+  base: "/wave-function-collapse/",
   plugins: [
     react({
         plugins: [
