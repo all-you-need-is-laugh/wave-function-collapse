be3d52ca6e49faea2a67b2f694e3945dd9ac8e8f
all-you-need-is-laugh <smerost2@gmail.com>
2025-04-18 12:17:57 +0200

    Refactor TileSet component to improve canvas rendering logic and handle edge cases for empty tiles

--- src/components/TileSet.tsx
+++ src/components/TileSet.tsx
@@ -11,39 +11,43 @@ const TileSet = ({ tiles }: TileSetProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!tiles.length) {
+    const firstTile = tiles[0];
+    if (!firstTile) {
       return;
     }
 
     const canvas = canvasRef.current;
-    if (canvas) {
-      const context = canvas.getContext('2d');
-      if (context) {
-        const pixelSize = 10;
-        const columns = Math.min(MAX_COLUMNS, Math.ceil(Math.sqrt(tiles.length)));
-        const rows = Math.ceil(tiles.length / columns);
-        const tileSize = tiles[0].size * pixelSize;
-        canvas.width = columns * tileSize;
-        canvas.height = rows * tileSize;
-
-        tiles.forEach((tile, tileIndex) => {
-          const xOffset = (tileIndex % columns) * tileSize;
-          const yOffset = Math.floor(tileIndex / columns) * tileSize;
-
-          tile.data.forEach((pixel, index) => {
-            const x = (index % tile.size) * pixelSize + xOffset;
-            const y = Math.floor(index / tile.size) * pixelSize + yOffset;
-            context.fillStyle = `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;
-            context.fillRect(x, y, pixelSize, pixelSize);
-          });
-
-          // Draw dotted border around each tile
-          context.strokeStyle = 'black';
-          context.setLineDash([5, 5]);
-          context.strokeRect(xOffset, yOffset, tileSize, tileSize);
-        });
-      }
+    if (!canvas) {
+      return;
     }
+    const context = canvas.getContext('2d');
+    if (!context) {
+      return;
+    }
+
+    const pixelSize = 10;
+    const columns = Math.min(MAX_COLUMNS, Math.ceil(Math.sqrt(tiles.length)));
+    const rows = Math.ceil(tiles.length / columns);
+    const tileSize = firstTile.size * pixelSize;
+    canvas.width = columns * tileSize;
+    canvas.height = rows * tileSize;
+
+    tiles.forEach((tile, tileIndex) => {
+      const xOffset = (tileIndex % columns) * tileSize;
+      const yOffset = Math.floor(tileIndex / columns) * tileSize;
+
+      tile.data.forEach((pixel, index) => {
+        const x = (index % tile.size) * pixelSize + xOffset;
+        const y = Math.floor(index / tile.size) * pixelSize + yOffset;
+        context.fillStyle = `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;
+        context.fillRect(x, y, pixelSize, pixelSize);
+      });
+
+      // Draw dotted border around each tile
+      context.strokeStyle = 'black';
+      context.setLineDash([5, 5]);
+      context.strokeRect(xOffset, yOffset, tileSize, tileSize);
+    });
   }, [tiles]);
 
   return <canvas ref={canvasRef} />;
